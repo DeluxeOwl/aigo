ecb6d59a9ddab8ac72d316eaebcafdda147c7682
DeluxeOwl <nospamplease0@protonmail.com>
2025-05-23 17:04:32 +0300

    refactor: base config

--- provider/providers.go
+++ provider/providers.go
@@ -2,6 +2,10 @@ package provider
 
 import "github.com/DeluxeOwl/aigo/provider/llm"
 
-func NewOllama(model llm.OllamaModel, options ...llm.OllamaOption) *llm.Ollama {
-	return llm.NewOllama(model, options...)
+func NewOllama(model llm.OllamaModel) *llm.Ollama {
+	return llm.NewOllama(model)
+}
+
+func NewOllamaWithConfig(model llm.OllamaModel, cfg llm.OllamaConfig) *llm.Ollama {
+	return llm.NewOllamaWithConfig(model, &cfg)
 }
