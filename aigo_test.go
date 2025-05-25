3661b80a52e436ebff76de0073a65096b81bfa72
DeluxeOwl <nospamplease0@protonmail.com>
2025-05-25 16:40:05 +0300

    feat: added ask hooks

--- aigo_test.go
+++ aigo_test.go
@@ -1,6 +1,8 @@
 package aigo_test
 
 import (
+	"context"
+	"strings"
 	"testing"
 
 	"github.com/DeluxeOwl/aigo"
@@ -11,9 +13,22 @@ import (
 func TestOllama(t *testing.T) {
 	ctx := t.Context()
 
-	resp, err := aigo.Ask(ctx, aigo.AskOptions{
+	resp, err := aigo.Ask(ctx, &aigo.AskOptions{
 		Provider: provider.NewOllama("qwen3:0.6B"),
-		Message:  "/no_think How are you?",
+		Message:  "/no_think What can you tell me about a fox?",
+		Hooks: &aigo.AskHooks{
+			BeforeAsk: []aigo.BeforeAsker{
+				aigo.BeforeAsk(func(_ context.Context, options *aigo.AskOptions) error {
+					options.Message = strings.ReplaceAll(options.Message, "fox", "bear")
+					return nil
+				}),
+			},
+			AfterAsk: []aigo.AfterAsker{
+				aigo.AfterAsk(func(_ context.Context, s string, err error) (string, error) {
+					return strings.ToUpper(s), err
+				}),
+			},
+		},
 	})
 	require.NoError(t, err)
 
--- ask.go
+++ ask.go
@@ -11,8 +11,50 @@ type Asker interface {
 type AskOptions struct {
 	Provider Asker
 	Message  string
+	Hooks    *AskHooks `exhaustruct:"optional"`
 }
 
-func Ask(ctx context.Context, options AskOptions) (string, error) {
-	return options.Provider.Ask(ctx, options.Message)
+type AskHooks struct {
+	BeforeAsk []BeforeAsker `exhaustruct:"optional"`
+	AfterAsk  []AfterAsker  `exhaustruct:"optional"`
+}
+
+type BeforeAsker interface {
+	BeforeAsk(ctx context.Context, options *AskOptions) error
+}
+type BeforeAsk func(ctx context.Context, options *AskOptions) error
+
+func (f BeforeAsk) BeforeAsk(ctx context.Context, options *AskOptions) error {
+	return f(ctx, options)
+}
+
+type AfterAsker interface {
+	AfterAsk(ctx context.Context, res string, err error) (string, error)
+}
+type AfterAsk func(ctx context.Context, res string, err error) (string, error)
+
+func (f AfterAsk) AfterAsk(ctx context.Context, response string, err error) (string, error) {
+	return f(ctx, response, err)
+}
+
+// TODO: what about hooks that always run?
+func Ask(ctx context.Context, options *AskOptions) (string, error) {
+	if options.Hooks != nil && len(options.Hooks.BeforeAsk) > 0 {
+		for _, h := range options.Hooks.BeforeAsk {
+			err := h.BeforeAsk(ctx, options)
+			if err != nil {
+				return "", err
+			}
+		}
+	}
+
+	res, err := options.Provider.Ask(ctx, options.Message)
+
+	if options.Hooks != nil && len(options.Hooks.AfterAsk) > 0 {
+		for _, h := range options.Hooks.AfterAsk {
+			res, err = h.AfterAsk(ctx, res, err)
+		}
+	}
+
+	return res, err
 }
