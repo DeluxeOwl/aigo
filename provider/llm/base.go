d91225fb3d532db135bd65afc4f53a3964a607e4
DeluxeOwl <nospamplease0@protonmail.com>
2025-06-05 09:26:48 +0300

    chore: comm

--- provider/llm/base.go
+++ provider/llm/base.go
@@ -7,6 +7,7 @@ import (
 	"github.com/DeluxeOwl/aigo/provider/schema"
 )
 
+// TODO: sealed interface so it must be embedded
 type Base struct {
 	client  *http.Client
 	url     string `exhaustruct:"optional"`
